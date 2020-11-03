9cdb83becae71a7cbe5d34257cd3d5d97eb05bad
Martin Sawilla <martin.sawilla@novatec-gmbh.de>
2020-11-03 16:16:54 +0100

    Start the REST API and the Webapps as a Servlet with Micronaut Servlet (on Jetty). Uses Jersey to get the JAX-RS Application as a Servlet.

---
+++ micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/rest/RestApp.java
@@ -0,0 +1,12 @@
+package info.novatec.micronaut.camunda.bpm.example.rest;
+
+import org.camunda.bpm.engine.rest.impl.CamundaRestResources;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class RestApp extends ResourceConfig {
+    public RestApp() {
+        // Register Camunda Rest Resources
+        registerClasses(CamundaRestResources.getResourceClasses());
+        registerClasses(CamundaRestResources.getConfigurationClasses());
+    }
+}
