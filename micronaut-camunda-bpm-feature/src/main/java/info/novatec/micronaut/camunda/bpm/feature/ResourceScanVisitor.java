1ee176da87c385b827b3c8c4802fcf71337f7cd8
Hayvon <aleksandrarshavskiy@gmail.com>
2020-11-10 13:14:56 +0100

    Added META-INF serivce for TypeElementVisitor

--- micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/ResourceScanVisitor.java
+++ micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/ResourceScanVisitor.java
@@ -1,38 +1,47 @@
 package info.novatec.micronaut.camunda.bpm.feature;
 
-import io.micronaut.core.annotation.AnnotationValue;
-import io.micronaut.core.annotation.AnnotationValueBuilder;
 import io.micronaut.inject.ast.ClassElement;
 import io.micronaut.inject.visitor.TypeElementVisitor;
 import io.micronaut.inject.visitor.VisitorContext;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Consumer;
 
 
 public class ResourceScanVisitor implements TypeElementVisitor<ResourceScan, Object> {
+
+    private static final Logger log = LoggerFactory.getLogger(ResourceScanVisitor.class);
+
+    @Override
+    public void start(VisitorContext visitorContext) {
+        log.info("Test1");
+        log.debug("Test2");
+        log.error("Test3");
+        System.err.println("Test4");
+
+        throw new RuntimeException();
+    }
 
     @Override
     public void visitClass(ClassElement element, VisitorContext context) {
        String test = "test";
 
+       log.info("Test1");
+       log.debug("Test2");
+       log.error("Test3");
+       System.err.println("Test4");
+
+       throw new RuntimeException();
+
            /*
         DO SCANNING HERE?
          */
-           //RuntimeExeption oder log
+
            //https://www.slideshare.net/graemerocher/micronaut-deep-dive-devoxx-belgium-2019 (Folie 36)
-        element.annotate(ResourceScan.class, resourceScanAnnotationValueBuilder -> {
+       /* element.annotate(ResourceScan.class, resourceScanAnnotationValueBuilder -> {
             element.stringValue(ResourceScan.class,test)
                     .ifPresent(resourceScanAnnotationValueBuilder::value);
-        });
+        });*/
 
         }
 }
