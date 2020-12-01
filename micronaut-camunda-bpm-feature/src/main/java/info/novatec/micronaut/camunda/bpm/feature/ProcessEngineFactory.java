d8e991f369d3caf22837e742fc621f4e642a9ab3
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2020-12-01 16:17:14 +0100

    WIP: Recursive

--- micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/ProcessEngineFactory.java
+++ micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/ProcessEngineFactory.java
@@ -3,6 +3,10 @@ package info.novatec.micronaut.camunda.bpm.feature;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Context;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.core.io.scan.ClassPathResourceLoader;
+import io.micronaut.core.io.scan.DefaultClassPathResourceLoader;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.slf4j.Logger;
@@ -11,7 +15,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Tobias Schäfer
@@ -33,11 +45,30 @@ public class ProcessEngineFactory {
      */
     @Context
     @Bean(preDestroy = "close")
-    public ProcessEngine processEngine(ProcessEngineConfiguration processEngineConfiguration, CamundaBpmVersion camundaBpmVersion) throws IOException {
+    public ProcessEngine processEngine(ProcessEngineConfiguration processEngineConfiguration, CamundaBpmVersion camundaBpmVersion, ResourceLoader resourceLoader, ClassPathResourceLoader defaultClassPathResourceLoader) throws IOException {
 
         log.info("Camunda BPM version: {}", camundaBpmVersion.getVersion());
 
         ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+
+        // 1.
+        Stream<URL> resources = resourceLoader.getResources("*.bpmn");
+        List<URL> collect = resources.collect(Collectors.toList());
+
+        // 2.
+        Stream<URL> resources1 = defaultClassPathResourceLoader.getResources("helloworld.bpmn");
+        List<URL> collect1 = resources1.collect(Collectors.toList());
+
+        // 3.
+        String folderPath = getClass().getResource("/").getPath();
+        Path p = Paths.get(folderPath);
+        Files.walk(p).forEach(x -> {
+            System.out.println(x);
+        });
+
+        // 4.
+        DefaultClassPathResourceLoader loader = (DefaultClassPathResourceLoader) new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
+        Optional<URL> resource = loader.getResource("classpath:helloworld.bpmn");
 
         deployProcessModels(processEngine);
 
@@ -53,7 +84,15 @@ public class ProcessEngineFactory {
      * @throws IOException if a resource, i.e. a model, cannot be loaded.
      */
     protected void deployProcessModels(ProcessEngine processEngine) throws IOException {
-        log.info("Searching non-recursively for models in the resources");
+        log.info("Searching recursively for models in the resources");
+        String folderPath = getClass().getResource("/").getPath();
+        Path p = Paths.get(folderPath);
+        Files.walk(p)
+                .filter( p1 -> p1.toString().endsWith("bpmn"))
+                .forEach( p2 -> {
+                    System.out.println(p2);
+                });
+
         PathMatchingResourcePatternResolver resourceLoader = new PathMatchingResourcePatternResolver();
         // Order of extensions has been chosen as a best fit for inter process dependencies.
         for (String extension : Arrays.asList("dmn", "cmmn", "bpmn")) {
