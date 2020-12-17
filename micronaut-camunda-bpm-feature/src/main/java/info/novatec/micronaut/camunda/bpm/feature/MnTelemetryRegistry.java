53cade7f3f6a4184614849bd3189a80e6d298344
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2020-12-17 15:22:28 +0100

    issue-122-bug-send-server-information

--- micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/MnTelemetryRegistry.java
+++ micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/MnTelemetryRegistry.java
@@ -1,5 +1,7 @@
 package info.novatec.micronaut.camunda.bpm.feature;
 
+import io.micronaut.http.server.netty.NettyHttpServer;
+import io.micronaut.runtime.server.EmbeddedServer;
 import org.camunda.bpm.engine.impl.telemetry.TelemetryRegistry;
 
 import javax.inject.Singleton;
@@ -13,6 +15,15 @@ import javax.inject.Singleton;
 public class MnTelemetryRegistry extends TelemetryRegistry {
 
     protected static final String INTEGRATION_NAME = "micronaut-camunda-bpm";
+
+    public MnTelemetryRegistry(EmbeddedServer embeddedServer) {
+        /*if (embeddedServer instanceof JettyServer) {
+            setApplicationServer("jetty/" + Jetty.VERSION);
+        } else*/ if (embeddedServer instanceof NettyHttpServer) {
+            io.netty.util.Version.identify().get()
+            setApplicationServer("netty/");
+        }
+    }
 
     @Override
     public String getCamundaIntegration() {
